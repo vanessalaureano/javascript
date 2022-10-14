2031cbe6d54b357884948d6f301fe7d5a110edc3
vanessalaureano <solytagamer@gmail.com>
2022-10-14 09:38:58 -0300

    criei ex09 js

--- ex09/ex09.js
+++ ex09/ex09.js
@@ -1,6 +1,9 @@
-var idade = 22
-if (idade < 18) {
-    console.log('Você é menor de idade.')
+var idade = 17
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota.')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto facultativo.')
 } else {
-    console.log('Você é maior de idade.')
+    console.log('Voto obrigatório.')
 }
