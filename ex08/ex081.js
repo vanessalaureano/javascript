bae88593baf327ad83992d63acdb2a2f2a5d6918
vanessalaureano <solytagamer@gmail.com>
2022-10-14 08:34:24 -0300

    Criei ex08 js

---
+++ ex08/ex081.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+if (vel > 60) { // Condição simples
+    console.log('Você ultrapassou o limite máximo de velocidade. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança.')
