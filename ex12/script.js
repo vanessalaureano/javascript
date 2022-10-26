c7cd5be9dd11adb2b0bc26edece9a1c12564fa77
vanessalaureano <solytagamer@gmail.com>
2022-10-26 15:23:33 -0300

    criei ex de contador

--- ex12/script.js
+++ ex12/script.js
@@ -1,1 +1,27 @@
-alert('Olá!')
+function contar() {
+    let ini = window.document.getElementById('iinicio')
+    let fim = window.document.getElementById('ifim')
+    let passo = window.document.getElementById('ipasso')
+    let res = window.document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`    
+    }
+}
